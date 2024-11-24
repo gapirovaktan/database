51139947dc6d3352c665a2eb1f2fe7c662b20620
gapirovaktan <150505604+gapirovaktan@users.noreply.github.com>
2024-11-24 17:30:26 +0600

    Update Main.java

--- Java/src/main/java/org/Main.java
+++ Java/src/main/java/org/Main.java
@@ -1,1 +1,33 @@
+package org.example;
 
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeData employeeData = new EmployeeData();
+
+        Employee emp1 = new Employee("Izat Manapov", "Mapper", 7300, new Date());
+        employeeData.createEmployee(emp1);
+        System.out.println("Employee have just created.");
+
+        Employee emp = employeeData.getEmployeeById(207109);
+        System.out.println("Retrieved: " + emp);
+
+        System.out.println("All employees:");
+        for (Employee e : employeeData.getAllEmployees()) {
+            System.out.println(e);
+        }
+
+        emp.setPosition("Tapper");
+        emp.setSalary(79000);
+        employeeData.updateEmployee(emp);
+        System.out.println("Employee updated: " + employeeData.getEmployeeById(1));
+
+        employeeData.deleteEmployee(1);
+        System.out.println("Employee deleted.");
+    }
+}
+            System.out.println(e.toString());
+        }
+    }
+}
